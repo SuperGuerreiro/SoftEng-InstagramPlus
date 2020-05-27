aac9410e70f158cd32c538cbf29d78420ef73228
Rafael Sequeira <rm.sequeira@campus.fct.unl.pt>
2020-05-27 20:30:05 +0100

    Added "login", fixed rword implementation
    Also changed to tabs, bye space gang

--- database/posts/posts.js
+++ database/posts/posts.js
@@ -1,5 +1,5 @@
 // posts estáticos
-posts_info_static ='{"posts":[{\
+posts_info_static = '{"posts":[{\
 "post_id":"static0",\
 "post_creation_ts":"-1",\
 "post_description":"Hello Friends,...",\
@@ -45,18 +45,18 @@ posts_info_static ='{"posts":[{\
 "author_avatar_path":"database/posts/content/static3.jpeg"\
 }]}';
 
-function create_post_key(post_id){
+function create_post_key(post_id) {
     return POST_KEY_PREFIX + post_id;
 }
 
-function is_post_key(key){
-    return !is_reserved(key) && key.startsWith(POST_KEY_PREFIX);
+function is_post_key(key) {
+    return key.startsWith(POST_KEY_PREFIX);
 }
 
 function load_static_posts() {
     console.log('Initializing posts db...');
     var posts = JSON.parse(posts_info_static).posts;
-    for(var i = 0; i < posts.length; i++)
+    for (var i = 0; i < posts.length; i++)
         localStorage.setItem(
             create_post_key(posts[i].post_id),
             JSON.stringify(posts[i]));
@@ -66,26 +66,26 @@ function load_static_posts() {
 
 // mesmo não sendo utilizado, pode ser interessante para debugging
 function get_all_posts() {
-    if(!is_loaded(POSTS_LOADED)) // lazy approach
+    if (!is_loaded(POSTS_LOADED)) // lazy approach
         this.load_static_posts();
 
     var posts = [], keys = Object.keys(localStorage);
-    for(var i = 0; i < keys.length; i++)
-        if(is_post_key(keys[i]))
+    for (var i = 0; i < keys.length; i++)
+        if (is_post_key(keys[i]))
             posts.push(JSON.parse(localStorage.getItem(keys[i])));
-    
+
     return posts;
 }
 
 function get_posts(posts_id) {
-    if(!is_loaded(POSTS_LOADED)) // lazy approach
+    if (!is_loaded(POSTS_LOADED)) // lazy approach
         this.load_static_users();
 
     posts = [];
-    for(var i = 0; i < posts_id.length; i++){
-       var post = JSON.parse(localStorage.getItem(create_post_key(posts_id[i])));
-       if(post != null)
-        posts.push(post);
+    for (var i = 0; i < posts_id.length; i++) {
+        var post = JSON.parse(localStorage.getItem(create_post_key(posts_id[i])));
+        if (post != null)
+            posts.push(post);
     }
 
     return posts;
--- database/users/users.js
+++ database/users/users.js
@@ -1,5 +1,5 @@
 // users estáticos
-users_info_static ='{"users":[\
+users_info_static = '{"users":[\
 {\
 "user_id":"superguerreiro666",\
 "user_name":"Renato João",\
@@ -51,55 +51,73 @@ users_info_static ='{"users":[\
 }\
 ]}';
 
-function create_user_key(user_id){
-    return USER_KEY_PREFIX + user_id;
+function create_user_key(user_id) {
+	return USER_KEY_PREFIX + user_id;
 }
 
-function is_user_key(key){
-    return !is_reserved(key) && key.startsWith(POST_KEY_PREFIX);
+function is_user_key(key) {
+	return key.startsWith(POST_KEY_PREFIX);
 }
 
 function load_static_users() {
-    console.log('Initializing users db...');
-    var users = JSON.parse(users_info_static).users;
-    for(var i = 0; i < users.length; i++)
-        localStorage.setItem(
-            create_user_key(users[i].user_id), 
-            JSON.stringify(users[i]));
-    
-    localStorage.setItem(USERS_LOADED, "1");
+	console.log('Initializing users db...');
+	var users = JSON.parse(users_info_static).users;
+	for (var i = 0; i < users.length; i++)
+		localStorage.setItem(
+			create_user_key(users[i].user_id),
+			JSON.stringify(users[i]));
+
+	localStorage.setItem(USERS_LOADED, "1");
 }
 
 // mesmo não sendo utilizado, pode ser interessante para debugging
 function get_all_users() {
-    if(!is_loaded(USERS_LOADED)) // lazy approach
-        this.load_static_users();
+	if (!is_loaded(USERS_LOADED)) // lazy approach
+		this.load_static_users();
 
-    var users = [], keys = Object.keys(localStorage);
-    for(var i = 0; i < keys.length; i++)
-        if(is_user_key(keys[i]))
-            users.push(JSON.parse(localStorage.getItem(keys[i])));
-    
-    return users;
+	var users = [], keys = Object.keys(localStorage);
+	for (var i = 0; i < keys.length; i++)
+		if (is_user_key(keys[i]))
+			users.push(JSON.parse(localStorage.getItem(keys[i])));
+
+	return users;
 }
 
 function get_users(users_id) {
-    if(!is_loaded(USERS_LOADED)) // lazy approach
-        this.load_static_users();
+	if (!is_loaded(USERS_LOADED)) // lazy approach
+		this.load_static_users();
 
-    users = [];
-    for(var i = 0; i < users_id.length; i++){
-       var user = JSON.parse(localStorage.getItem(create_user_key(users_id[i])));
-       if(user != null)
-        users.push(user);
-    }
-
-    return users;
-}
-
-// devolve um utilizador com id user_id ou null se não existir
-function get_user(user_id) {
-    return get_users([user_id]);
+	users = [];
+	for (var i = 0; i < users_id.length; i++) {
+		var user = JSON.parse(localStorage.getItem(create_user_key(users_id[i])));
+		if (user != null)
+			users.push(user);
+	}
+	return users;
 }
 
 
+/**
+ * login
+ * 
+ * está aqui pq dá jeito e já tinha depêndencias
+ * não tem nada a ver com a arquitetura tbh
+ * 
+ */
+
+// define um utilizador como logado
+function set_current_user(user_id) {
+	if (get_users([user_id]).length <= 0)
+		return;
+	localStorage.setItem(CURRENT_USER_LABEL, user_id);
+}
+
+// devolve o utilizador logado
+function get_current_user() {
+	return get_users([get_current_user_id()])[0];
+}
+
+// devolve o id do utilizador logado
+function get_current_user_id() {
+	return localStorage.getItem(CURRENT_USER_LABEL);
+}
