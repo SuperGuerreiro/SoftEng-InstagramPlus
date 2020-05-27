d31d2b6238690eef1df5fb26d50221ee4b4d0ba0
Rafael Sequeira <rm.sequeira@campus.fct.unl.pt>
2020-05-27 23:47:30 +0100

    Added dynamic feed, not tested tho

--- server/feed_controller.js
+++ server/feed_controller.js
@@ -1,5 +1,14 @@
 // retorna a informação sobre o feed
 function get_feed() {
-    // faz pedido à base de dados de posts
-    return get_all_posts();
+    var current_user = get_current_user();
+    if (current_user == null) {
+        console.log("not logged in")
+        return;
+    }
+
+    var following_users = get_users(current_user.user_following), feed_posts = [];
+    for (var i = 0; i < following_users.user_following; i++)
+        feed_posts.concat(get_posts(following_users[i].user_posts));
+        
+    return feed_posts;
 };
--- server/profile_controller.js
+++ server/profile_controller.js
@@ -1,15 +1,20 @@
+
+
 function get_profile(user_id){
-    current_user_id = get_current_user_id();
+    var current_user_id = get_current_user_id();
     if(current_user_id == null){
         console.log("not logged in")
         return;
     }
-    var user = get_users([user_id])[0], posts = [];
 
-    if(user.user_profile_visibility > 0 || // current user can view profile
-        user.user_followers.indexOf(current_user_id) >= 0 || current_user_id == user_id )
+    var user = get_users([user_id])[0], posts = [];
+    if(user.user_profile_visibility > 0 || // público
+        user.user_followers.indexOf(current_user_id) >= 0 || // seguido pelo user
+         current_user_id == user_id ) // próprio user
              posts = get_posts(user.user_posts);
-    
+
     return [user, posts];
 }
+
+
 
