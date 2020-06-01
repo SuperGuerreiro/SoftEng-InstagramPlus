dc311d69ec02f1da1caa56064f8df70c67bd7918
Rafael Sequeira <rm.sequeira@campus.fct.unl.pt>
2020-06-01 01:45:35 +0100

    Linkácios para perfis adicionados

--- client/load_feed.js
+++ client/load_feed.js
@@ -1,3 +1,6 @@
+const PROFILE_URL = "./Components/Profile/profile.html";
+const STRANGE_PROFILE_URL = "./Components/strange_profile/strange_profile.html";
+
 // o html de cada post, irá ser carregado dinamicamente
 var post_template =
 	'<article class="post">' +
@@ -6,18 +9,20 @@ var post_template =
 	'<img src="{0}"/>' + //usr_avatar_path
 	'</div>' +
 	'<div class="post-user-nickname">' +
-	'<span>{1}</span>' + //usr_username
+	'<a class=profile-url href="{1}"' +
+	'<span>{2}</span>' + //usr_username
+	'</a>' +
 	'</div>' +
 	'</div>' +
 	'<div class="post-content">' +
 	'<div class="post-content-bg">' +
-	'<figure class="{2}">' + // pst_filter
-	'<img src="{3}"/>' + // pst_content_path
+	'<figure class="{3}">' + // pst_filter
+	'<img src="{4}"/>' + // pst_content_path
 	'</figure>' +
 	'</div>' +
 	'</div>' +
 	'<div class="post-caption">' +
-	'<strong>{4}</strong> {5}' + //usr_username, pst_description
+	'<strong>{5}</strong> {6}' + //usr_username, pst_description
 	'</div>' +
 	'</article>'
 
@@ -25,8 +30,14 @@ function load_feed(posts) {
 	var feed = $("#psts");
 	for (i = 0; i < posts.length; i++) {
 		var post = posts[i];
+		
+		var profile_href = STRANGE_PROFILE_URL;
+		if(post.author_username == get_loggedin_user_id())
+			profile_href = PROFILE_URL;
+
 		feed.append(post_template.format(
 			post.author_avatar_path,
+			profile_href,
 			post.author_username,
 			post.post_filter,
 			post.post_content_path,
