46cea815df43c75f1f29847b1e5489d36391f0d8
Rafael Sequeira <rm.sequeira@campus.fct.unl.pt>
2020-06-02 03:02:45 +0100

    Adicionei hashtags como link, arranjei bug

--- client/load_feed.js
+++ client/load_feed.js
@@ -9,7 +9,7 @@ var post_template =
 	'<img src="{0}"/>' + //usr_avatar_path
 	'</div>' +
 	'<div class="post-user-nickname">' +
-	'<a class=profile-url href="{1}"' +
+	'<a class=profile-url href="{1}">' +
 	'<span>{2}</span>' + //usr_username
 	'</a>' +
 	'</div>' +
@@ -22,16 +22,37 @@ var post_template =
 	'</div>' +
 	'</div>' +
 	'<div class="post-caption">' +
-	'<strong>{5}</strong> {6}' + //usr_username, pst_description
+	'<strong>{5}</strong>' + // usr_username,
+	' {6}' + // pst_description
 	'</div>' +
-	'</article>'
+	'</article>';
+
+var hashtag_ref_template =
+	'<a class=hashtag-profile-url href="{0}">' +
+	'{1}' + //hashtag
+	'</a>';
+
+
+function build_description(description, hashtags) {
+	for (var i = 0; i < hashtags.length; i++) {
+		description = description.replace('#' + hashtags[i],
+			hashtag_ref_template.format(
+				"teste",
+				'#' + hashtags[i]
+			));
+			console.log("replaced");
+	}
+	console.log(description);
+	return description;
+}
+
 
 function load_feed(posts) {
 	var feed = $("#psts");
 	for (i = 0; i < posts.length; i++) {
 		var post = posts[i];
 
-		var profile_href = STRANGE_PROFILE_URL;
+		var profile_href = STRANGE_PROFILE_URL; // lucky guess 2.0?
 		if (post.author_username == get_loggedin_user_id())
 			profile_href = PROFILE_URL;
 
@@ -42,16 +63,13 @@ function load_feed(posts) {
 			post.post_filter,
 			post.post_content_path,
 			post.author_username,
-			post.post_description
+			build_description(post.post_description, post.post_hashtags)
 		));
 	}
 }
 
 window.onload = function () {
 	this.set_loggedin_user(LOGGEDIN_USER) // isto pode ser feito de maneira mais inteligente
-
-	// faz request ao "servidor" (feed_controller) para obter os posts
-	// carrega os posts no feed
 	this.load_feed(this.get_feed());
 
 	//"event listener" em todos os links
