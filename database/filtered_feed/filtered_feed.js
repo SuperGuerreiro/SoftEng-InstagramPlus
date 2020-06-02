e4fa70bf9c86bae5c74c8af8d38cc114eabd0bd5
Rafael Sequeira <rm.sequeira@campus.fct.unl.pt>
2020-06-02 17:34:45 +0100

    Bug massivo fixado

--- database/filtered_feed/filtered_feed.js
+++ database/filtered_feed/filtered_feed.js
@@ -5,8 +5,8 @@ fls_info_static = '{"filter_lists":['
 "fl_description":"1590507192000",\
 "fl_name":"Que fofura... #cute #puppy",\
 "fl_owner_id":"//TODO",\
-"fl_hashtags":["cute"],\
-"fl_users_id":["rsantos"],\
+"fl_hashtags":["funny","cute"],\
+"fl_users_id":[]\
 }'
     +
     ']}';
@@ -15,7 +15,7 @@ function create_fl_key(post_id) {
     return FL_KEY_PREFIX + post_id;
 }
 
-function is_post_key(key) {
+function is_fl_key(key) {
     return key.startsWith(FL_KEY_PREFIX);
 }
 
@@ -36,9 +36,10 @@ function get_fls(fls_id) {
 
     fls = [];
     for (var i = 0; i < fls_id.length; i++) {
-        var fl = localStorage.getItem(create_post_key(fls_id[i]));
+        var fl = localStorage.getItem(create_fl_key(fls_id[i]));
         if (fl != null)
             fls.push(JSON.parse(fl));
     }
+
     return fls;
 }
