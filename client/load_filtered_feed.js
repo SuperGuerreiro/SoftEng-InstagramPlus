57c101fa967d7f00a6472a2e045125259bed80cd
Rafael Sequeira <rm.sequeira@campus.fct.unl.pt>
2020-06-02 22:12:00 +0100

    BUGÁCIO ESMAGÁCIO 3 (mereço ser exterminado)

--- client/load_filtered_feed.js
+++ client/load_filtered_feed.js
@@ -1,4 +1,4 @@
-const PROFILE_URL = ".../../Components/Profile/profile.html";
+const PROFILE_URL = "../../Components/Profile/profile.html";
 const STRANGE_PROFILE_URL = "../../Components/strange_profile/strange_profile.html";
 const HASHTAG_PROFILE_URL = "../../Components/hashtag_profile/hashtag_profile.html";
 const PATH = "../../";
